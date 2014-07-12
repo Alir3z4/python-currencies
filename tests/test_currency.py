a165adcab0cabf8e7176d93575721e009c934c06
Alireza Savand <alireza.savand@gmail.com>
2014-07-12 18:23:01 +0400

    Add test_get_money_currency

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -1,5 +1,10 @@
 from unittest import TestCase
+from currencies import Currency
 
 
 class TestCurrency(TestCase):
-    pass
+    def test_get_money_currency(self):
+        currency = Currency('USD')
+
+        self.assertIsInstance(currency.get_money_currency(), str)
+        self.assertEqual(currency.get_money_currency(), 'USD')
