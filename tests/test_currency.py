c262cafdfff51a3176e5a9ee998be5ec4f8b545a
Alireza Savand <alireza.savand@gmail.com>
2014-07-12 18:23:24 +0400

    Add test_get_currency_formats

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -8,3 +8,10 @@ class TestCurrency(TestCase):
 
         self.assertIsInstance(currency.get_money_currency(), str)
         self.assertEqual(currency.get_money_currency(), 'USD')
+
+    def test_get_currency_formats(self):
+        currency_formats = Currency.get_currency_formats()
+
+        self.assertIsNotNone(currency_formats)
+        self.assertIsInstance(currency_formats, list)
+        self.assertGreater(len(currency_formats), 0)
