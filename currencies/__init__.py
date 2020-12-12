7418526ed9db7d14d147bb2a3dc16187d4ef7733
Alireza Savand <591113+Alir3z4@users.noreply.github.com>
2020-12-12 19:40:17 +0300

    Release 2020.12.12 (#9)

--- currencies/__init__.py
+++ currencies/__init__.py
@@ -1,54 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
+from typing import Union, List, Optional
 from currencies.config import MONEY_FORMATS
 from currencies.exceptions import CurrencyDoesNotExist
 
-__VERSION__ = (2020, 1, 12)
+__VERSION__ = (2020, 12, 12)
 
 
-def get_version():
-    """
-    :rtype: basestring
-    """
+def get_version() -> str:
+    """Returns the package version."""
     return ".".join(str(v) for v in __VERSION__)
 
 
-class Currency(object):
-    money_currency = None
+class Currency:
+    money_currency: Optional[str] = None
     money_formats = MONEY_FORMATS
 
-    def __init__(self, money_currency):
-        """
-        :type money_currency: str
-        """
+    def __init__(self, money_currency: str):
         self.set_money_currency(money_currency)
 
-    def set_money_currency(self, money_currency):
-        """
-        :type money_currency: str
-        """
+    def set_money_currency(self, money_currency: str) -> None:
         if money_currency not in self.money_formats:
             raise CurrencyDoesNotExist
 
         self.money_currency = money_currency
 
-    def get_money_currency(self):
-        """
-        :rtype: str
-        """
+    def get_money_currency(self) -> str:
         return self.money_currency
 
     @classmethod
-    def get_currency_formats(cls):
-        """
-        :rtype: list
-        """
+    def get_currency_formats(cls) -> list:
         return list(cls.money_formats.keys())
 
-    def get_money_format(self, amount):
+    def get_money_format(self, amount: Union[int, float, str]) -> str:
         """
-        :type amount: int or float or str
-
         Usage:
         >>> currency = Currency('USD')
         >>> currency.get_money_format(13)
@@ -57,17 +42,13 @@ class Currency(object):
         >>> '$13.99'
         >>> currency.get_money_format('13,2313,33')
         >>> '$13,2313,33'
-
-        :rtype: str
         """
         return self.money_formats[
             self.get_money_currency()
         ]['money_format'].format(amount=amount)
 
-    def get_money_with_currency_format(self, amount):
+    def get_money_with_currency_format(self, amount: Union[int, float, str]) -> str:
         """
-        :type amount: int or float or str
-
         Usage:
         >>> currency = Currency('USD')
         >>> currency.get_money_with_currency_format(13)
@@ -76,8 +57,6 @@ class Currency(object):
         >>> '$13.99 USD'
         >>> currency.get_money_with_currency_format('13,2313,33')
         >>> '$13,2313,33 USD'
-
-        :rtype: str
         """
         return self.money_formats[
             self.get_money_currency()
