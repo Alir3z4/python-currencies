c9976b2aff2e670e1cb8ed1905df5daecb56ab74
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-21 18:42:43 +0100

    Add Python 3.9 support and update PyPI classifiers

--- setup.py
+++ setup.py
@@ -40,7 +40,10 @@ setup(
         'Operating System :: OS Independent',
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development"
     ],
     python_requires='<4',
